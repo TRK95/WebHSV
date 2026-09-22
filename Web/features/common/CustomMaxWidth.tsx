1986bbdb15191f3da0a5f9e20cb6f9deca6cf4e1
TRK95_nguyetanh <86118897+TRK95@users.noreply.github.com>
2026-09-23 01:01:16 +0700

    Fix web dropdown and page width

--- Web/features/common/CustomMaxWidth.tsx
+++ Web/features/common/CustomMaxWidth.tsx
@@ -2,9 +2,9 @@ import { useTheme, useMediaQuery } from '@mui/material'
 
 const customMaxWidthContainer = () => {
     const theme = useTheme()
-    const isLgDesktopUI = useMediaQuery(theme.breakpoints.down('xxl'))
+    const isBelowXxl = useMediaQuery(theme.breakpoints.down('xxl'))
 
-    return isLgDesktopUI ? 'lg' : 'xl'
+    return isBelowXxl ? 'xl' : 'xxl'
 }
 
 export default customMaxWidthContainer
